a57361021b606f6fc6b87fee1c9b25b8d5b24595
Rakesh <rb610883@gmail.com>
2021-04-14 11:20:27 +0530

    rkdj

---
+++ homeControle.js
@@ -0,0 +1,11 @@
+function demonsfunc(){
+    var demons = document.getElementById("_demons")
+    a = demons.setAttribute('src','Videos/Demons.mp4')
+    document.body.appendChild(demons);
+}
+function play(){
+    document.getElementById("_believer").innerHTML.setAttribute('src','Videos/believer.mp4');
+}
+function showslide(){
+    
+}
